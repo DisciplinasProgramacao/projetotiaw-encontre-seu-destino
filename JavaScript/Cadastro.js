59497b73d658c6f8422761220cddad859a225d69
pauloaguiari <61600486+pauloaguiari@users.noreply.github.com>
2022-11-19 17:59:19 -0300

    Alert Provisório

--- JavaScript/Cadastro.js
+++ JavaScript/Cadastro.js
@@ -2,11 +2,23 @@ function  cadastro(){
 localStorage.nome = document.getElementById("nome_cad").value 
 localStorage.email = document.getElementById("email_cad").value 
 localStorage.senha = document.getElementById("senha_cad").value 
+    
+    if(senha_cad == null)
+alert("campo vazio")
+
+else(senha_cad == senha_cad)
+alert("cadastro realizado")
 
 }
 
 function Logar(){
-    document.getElementById("email_cad").value = localStorage.email
-    document.getElementById("senha_cad").value = localStorage.senha
+  document.getElementById("email_login").value = localStorage.email
+    document.getElementById("senha_login").value = localStorage.senha
+
+    if(senha_login == null)
+alert("campo vazio")
+
+else(senha_login == senha_login)
+alert("Login realizado com sucesso")
 }
 
