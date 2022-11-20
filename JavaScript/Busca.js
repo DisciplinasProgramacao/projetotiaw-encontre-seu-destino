aefc149cedc9e372cb19962c81969f9b6834415c
Rafael Silva <85958187+RAFX99@users.noreply.github.com>
2022-11-20 10:57:46 -0300

    Add files via upload

--- JavaScript/Busca.js
+++ JavaScript/Busca.js
@@ -1,31 +1,91 @@
-var Lugares = ['Rio de Janeiro', 'Rio Grande do Sul', 'São Paulo', 'Bahia'];
+var RJ = ["rj", "Rio de Janeiro", "rio"]
+var SP = ["sp", "São Paulo", "são paulo"]
+var MG = ["mg", "minas", "minas gerais"]
+var MA = ["ma", "maranhão", "maranhao"]
+var MT = ["mt", "mato grosso", "mato"]
+var PE = ["pe", "pernambuco", "pernam"]
+var SC = ["sc", "santa catarina", "santa"]
+var RS = ["rs", "rio grande do sul", "rio", "sul"]
+var RR = ["rr", "roraima", "rorai"]
+var PR = ["pr", "parana", "para"]
 
-const content = document.querySelector(".content");
-const inputSearch = document.querySelector("input[type='search']");
+const btn = document.getElementById('click', (e) => {
 
-let items = [];
+  for(var i = 0; i < RJ.length; i++){
+    if(RJ[i] == valordovetor){
+      window.location(rj.html)
+    }
+  }
 
-inputSearch.oninput = () => {
-  content.innerHTML = "";
+  
+})
 
-  items
-    .filter((item) =>
-      item.toLowerCase().includes(inputSearch.value.toLowerCase())
-    )
-    .forEach((item) => addHTML(item));
-};
 
-function addHTML(item) {
-  const div = document.createElement("div");
-  div.innerHTML = item;
-  content.append(div);
-}
+function pesquisar()
+{
+  
+var valorInput = document.getElementById("inputPesquisa").value;
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === RJ[i]){
+      alert("Achamos o " + RJ[1])
+    }
+  }
 
-fetch(Lugares)
-  .then((data) => data.json())
-  .then((users) => {
-    users.forEach((user) => {
-      addHTML(user.name);
-      items.push(user.name);
-    });
-  });
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === SP[i]){
+      alert("Achamos São Paulo!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === MG[i]){
+      alert("Achamos Minas Gerais!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === MA[i]){
+      alert("Achamos Maranhão!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === MT[i]){
+      alert("Achamos Mato Grosso!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === PE[i]){
+      alert("Achamos Pernambuco!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === SC[i]){
+      alert("Achamos Santa Catarina!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === RS[i]){
+      alert("Achamos Rio Grande do Sul!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === RR[i]){
+      alert("Achamos Roraima!")
+    }
+  }
+
+  for(let i = 0; i < 3; i++){
+    if(valorInput.toLowerCase() === PR[i]){
+      alert("Achamos Paraná!")
+    }
+    
+  }
+
+  
+  }
+  
