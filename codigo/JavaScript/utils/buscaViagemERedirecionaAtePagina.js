790f024ec21fabd4c52991f6dad1f244d8dd70aa
Rafael Silva <85958187+RAFX99@users.noreply.github.com>
2022-12-14 23:17:17 -0300

    Delete codigo/JavaScript/utils directory

--- codigo/JavaScript/utils/buscaViagemERedirecionaAtePagina.js
+++
@@ -1,9 +0,0 @@
-function buscaViagemERedirecionaAtePagina(inputPesquisador, arrayCidades) {
-    const valorPesquisado = inputPesquisador.value;
-
-    arrayCidades.forEach(cidade => {
-        if (valorPesquisado === cidade.cidade) {
-           return window.location.assign(`${cidade.link}.html`)
-        }
-    });
-}
